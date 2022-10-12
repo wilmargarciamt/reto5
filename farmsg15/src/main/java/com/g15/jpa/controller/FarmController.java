467e58a9d668fb2b45e4426e4d7ed2381a5bdcd5
Crhistian Ovalle <covalle@desarrolloextemo.com.co>
2022-10-12 18:56:28 -0500

    Update FarmController.java

--- farmsg15/src/main/java/com/g15/jpa/controller/FarmController.java
+++ farmsg15/src/main/java/com/g15/jpa/controller/FarmController.java
@@ -5,6 +5,7 @@ import com.g15.jpa.service.FarmService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/Farm")
+@CrossOrigin(origins = "*")
 public class FarmController {
     @Autowired
     private FarmService farmService;
