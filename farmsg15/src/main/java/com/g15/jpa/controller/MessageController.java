ba3f8c68bcd0523f40e865ece8676e11ff4e6481
Crhistian Ovalle <covalle@desarrolloextemo.com.co>
2022-10-12 18:57:06 -0500

    Update MessageController.java

--- farmsg15/src/main/java/com/g15/jpa/controller/MessageController.java
+++ farmsg15/src/main/java/com/g15/jpa/controller/MessageController.java
@@ -5,6 +5,7 @@ import com.g15.jpa.service.MessageService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/Message")
+@CrossOrigin(origins = "*")
 public class MessageController {
 
     @Autowired
