fadd4dc67d6810d33706e711c2dd1b2ab5f1ee15
Crhistian Ovalle <covalle@desarrolloextemo.com.co>
2022-10-12 18:55:32 -0500

    Update CategoryController.java

--- farmsg15/src/main/java/com/g15/jpa/controller/CategoryController.java
+++ farmsg15/src/main/java/com/g15/jpa/controller/CategoryController.java
@@ -6,6 +6,7 @@ import com.g15.jpa.service.CategoryService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/Category")
+@CrossOrigin(origins = "*")
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
