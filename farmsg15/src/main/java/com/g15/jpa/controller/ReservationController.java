ff870d10da80e981c3a95e4964d0f3d25c01b3c5
Crhistian Ovalle <covalle@desarrolloextemo.com.co>
2022-10-12 18:57:39 -0500

    Update ReservationController.java

--- farmsg15/src/main/java/com/g15/jpa/controller/ReservationController.java
+++ farmsg15/src/main/java/com/g15/jpa/controller/ReservationController.java
@@ -5,6 +5,7 @@ import com.g15.jpa.service.ReservationService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/Reservation")
+@CrossOrigin(origins = "*")
 public class ReservationController {
     @Autowired
     private ReservationService reservationService;
